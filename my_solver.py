86a45a52e74f7e3db2cdcf1a2dd4e4f5e366c738
minh702 <minh702@korea.ac.kr>
2023-10-15 17:10:11 +0900

    fixed edge-edge collision

--- my_solver.py
+++ my_solver.py
@@ -30,7 +30,7 @@ class Solver:
         self.num_edges_static = len(self.edges_static)
         self.faces_static = self.static_mesh.mesh.faces
 
-        self.radius = 0.005
+        self.radius = 0.001
         self.contact_stiffness = 1e3
         self.damping_factor = 0.001
         self.grid_n = 128
@@ -106,40 +106,70 @@ class Solver:
         cd = d - c
         ac = c - a
 
-        mat = ti.math.mat2([[-cd.dot(ab), ab.dot(ab)],
-                            [-cd.dot(cd), cd.dot(ab)]])
+        a11 = ab.dot(ab)
+        a12 = -cd.dot(ab)
+        a21 = cd.dot(ab)
+        a22 = -cd.dot(cd)
+        det = a11 * a22 - a12 * a21
+        mat = ti.math.mat2([[ab.dot(ab), -cd.dot(ab)], [cd.dot(ab), -cd.dot(cd)]])
 
         #
         gg = ti.math.vec2([ab.dot(ac), cd.dot(ac)])
 
+        t = ti.math.vec2([0.0, 0.0])
+        if abs(det) > 1e-4:
+            t = mat.inverse() @ gg
 
-        t = mat.inverse() @ gg
+
+        #
+        # s = (a22 * gg[0] - a12 * gg[1]) / det
+        # t = (-a21 * gg[0] + a11 * gg[1]) / det
+        # t2 = ti.min(1, ti.max(t2, 0))
 
         t1 = t[0]
-        # t1 = ti.min(1, ti.max(t1, 0))
-
         t2 = t[1]
-        # t2 = ti.min(1, ti.max(t2, 0))
-        self.p[0] = a + t1 * ab
-        self.p[1] = c + t2 * cd
 
-        dist = (self.p[0] - self.p[1]).norm()
+        if t1 < 0.0:
+            t1 = 0.0
+
+        if t1 > 1.0:
+            t1 = 1.0
+
+        if t2 < 0.0:
+            t2 = 0.0
+
+        if t2 > 1.0:
+            t2 = 1.0
+
+        p1 = a + t1 * ab
+        p2 = c + t2 * cd
+
+
+        # self.p[0] = p1
+        # self.p[1] = p2
+        # p01 = self.p[0] - self.p[1]
+
+        # print(f'{p01.dot(ab)}, {p01.dot(cd)}')
+
+        dist = (p1 - p2).norm()
+        # print(dist)
         tol = 1e-2
         if dist < tol:
-            print("test")
-            # C = 0.5 * (dist - tol) ** 2
-            # n = (p1 - p2).normalized(1e-6)
-            # nablaC_a = (dist - tol) * t1 * n
-            # nablaC_b = (dist - tol) * (1 - t1) * n
-            # Schur = nablaC_a.dot(nablaC_a) / e.verts[0].m + nablaC_b.dot(nablaC_b) / e.verts[1].m
-            # ld = 0.0
-            # if Schur > 1e-4:
-            #     ld = C / Schur
-            #
-            # e.verts[0].g += ld * nablaC_a
-            # e.verts[1].g += ld * nablaC_b
-            # e.verts[0].h += ld
-            # e.verts[1].h += ld
+            # print("test")
+            C = 0.5 * (dist - tol) ** 2
+            n = (p1 - p2).normalized(1e-6)
+            nablaC_a = (dist - tol) * t1 * n
+            nablaC_b = (dist - tol) * (1 - t1) * n
+            Schur = nablaC_a.dot(nablaC_a) / self.verts.m[1] + nablaC_b.dot(nablaC_b) / self.verts.m[2]
+
+            kc = 1e3
+
+            ld = C / Schur
+
+            self.verts.g[1] += ld * nablaC_a
+            self.verts.g[2] += ld * nablaC_b
+            self.verts.h[1] += ld
+            self.verts.h[2] += ld
 
 
         for v in self.verts:
