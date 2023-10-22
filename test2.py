33399e249f1c29120eabacc43f366f3c941609d5
mhkee <minh702@korea.ac.kr>
2023-10-22 20:52:01 +0900

    upgrade sqp

--- test2.py
+++ test2.py
@@ -8,7 +8,7 @@ vec = ti.math.vec3
 SAVE_FRAMES = False
 
 window_size = 1024  # Number of pixels of the window
-dt = 0.003  # Larger dt might lead to unstable results.
+dt = 0.002  # Larger dt might lead to unstable results.
 
 # mesh = Mesh("obj_models/cube.obj", scale=0.1, rot=ti.math.vec3(90.0, 0.0, 0.0),trans=ti.math.vec3(0.3, 0.5, 0.3))
 # static_mesh =Mesh("obj_models/cube.obj", scale=0.1, rot=ti.math.vec3(180.0, 0.0, 0.0), trans=ti.math.vec3(0.3, 0.2, 0.3))
@@ -35,7 +35,7 @@ dt = 0.003  # Larger dt might lead to unstable results.
 
 #case: face vs. face
 # mesh = Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(0.0, 0.0, 0.0),trans=ti.math.vec3(0.3, 0.6, 0.3))
-# static_mesh = Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(180.0, 0.0, 45.0), trans=ti.math.vec3(0.3, 0.2, 0.3))
+# static_mesh = Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(180.0, 0.0, 50.0), trans=ti.math.vec3(0.3, 0.2, 0.3))
 
 # mesh = Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(0.0, 0.0, 0.0),trans=ti.math.vec3(0.3, 0.6, 0.3))
 # static_mesh = Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(180.0, 0.0, 0.0), trans=ti.math.vec3(0.3, 0.2, 0.3))
@@ -49,8 +49,8 @@ dt = 0.003  # Larger dt might lead to unstable results.
 # static_mesh = Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(0.0, 0.0, 0.0), trans=ti.math.vec3(0.3, 0.2, 0.3))
 
 # case: vertex vs. face
-mesh = Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(0.0, 0.0, 0.0),trans=ti.math.vec3(0.3, 0.9, 0.3))
-static_mesh =Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(0.0, 0.0, 0.0), trans=ti.math.vec3(0.3, 0.2, 0.3))
+# mesh = Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(0.0, 0.0, 0.0),trans=ti.math.vec3(0.3, 0.9, 0.3))
+# static_mesh =Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(0.0, 0.0, 0.0), trans=ti.math.vec3(0.3, 0.2, 0.3))
 
 # mesh = Mesh("obj_models/square_big.obj", scale=0.05, rot=ti.math.vec3(0.0, 0.0, 0.0),trans=ti.math.vec3(0.5, 0.8, 0.5))
 # static_mesh =Mesh("obj_models/cube.obj", scale=0.1, rot=ti.math.vec3(0.0, 0.0, 0.0), trans=ti.math.vec3(0.5, 0.21, 0.5))
@@ -101,10 +101,9 @@ static_mesh =Mesh("obj_models/tetrahedron.obj", scale=0.3, rot=ti.math.vec3(0.0,
 # mesh = Mesh("obj_models/triangle.obj", scale=0.5, rot=ti.math.vec3(90, 0.0, 0.0),trans=ti.math.vec3(1.5, 0.7, 0.6))
 # static_mesh = Mesh("obj_models/triangle.obj", scale=0.5, rot=ti.math.vec3(0.0, 0.0, 0.0),trans=ti.math.vec3(0.5, 0.1, 0.5))
 #
-#
 
-# mesh = Mesh("obj_models/square_big.obj", scale=0.05, rot=ti.math.vec3(0.0, 0.0, 20.0), trans=ti.math.vec3(0.5, 0.8, 0.5))
-# static_mesh =Mesh("obj_models/square_big.obj", scale=0.08, rot=ti.math.vec3(0.0, 20.0, 0.0), trans=ti.math.vec3(0.5, 0.21, 0.5))
+mesh = Mesh("obj_models/square_big.obj", scale=0.05, rot=ti.math.vec3(0.0, 0.0, 20.0), trans=ti.math.vec3(0.5, 0.8, 0.5))
+static_mesh =Mesh("obj_models/square_big.obj", scale=0.08, rot=ti.math.vec3(0.0, 20.0, 0.0), trans=ti.math.vec3(0.5, 0.21, 0.5))
 
 per_vertex_color = ti.Vector.field(3, ti.float32, shape=4)
 debug_edge_indices = ti.field(dtype=ti.i32, shape=2)
