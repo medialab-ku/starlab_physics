d0d8cff87a8168bb46d7bd834ece41ed22995f84
minh702 <minh702@korea.ac.kr>
2023-10-28 22:36:41 +0900

    include collision handling in pcg

--- utils.py
+++ utils.py
@@ -4,17 +4,39 @@ import taichi as ti
 ti.init(arch=ti.gpu)
 
 S = ti.root.dynamic(ti.i, 1024, chunk_size=32)
-SphereType = ti.types.struct(center=ti.math.vec3, radius=float)
-x = SphereType.field()
+mat9x9 = ti.types.matrix(n=9, m=9, dtype=ti.f32)
+x = mat9x9.field()
 S.place(x)
 
-test = ti.Matrix.field
+@ti.func
+def test2(i: ti.f32):
+    mat = ti.Matrix([[i, 0, 0, 0, 0, 0, 0, 0, 0],
+                     [0, i, 0, 0, 0, 0, 0, 0, 0],
+                     [0, 0, i, 0, 0, 0, 0, 0, 0],
+                     [0, 0, 0, i, 0, 0, 0, 0, 0],
+                     [0, 0, 0, 0, i, 0, 0, 0, 0],
+                     [0, 0, 0, 0, 0, i, 0, 0, 0],
+                     [0, 0, 0, 0, 0, 0, i, 0, 0],
+                     [0, 0, 0, 0, 0, 0, 0, i, 0],
+                     [0, 0, 0, 0, 0, 0, 0, 0, i]])
+
+    return mat
+
+
+# ti.linalg.taichi_cg_solver()
 
 @ti.kernel
 def test():
+   center = ti.math.vec3(0.5, 0.5, 0.5)
 
-    x.append(SphereType(center=ti.math.vec3(1, 1, 1), radius=0.1))
-    print(x.length())
-    print(x[0].center)
+   vec = ti.math.vec3(0.5, 0.8, 0.5)
+
+   rad = 0.4
+
+   normal = (vec - center).normalized()
+
+   p = center + rad * normal
+
+   print(p)
 
 test()
