52534c9feffcf16be99a3bcc8c2cfe4ce5033669
minh702 <minh702@korea.ac.kr>
2024-07-12 16:37:21 +0900

    test module. compute adjacency list

--- framework/test.py
+++ framework/test.py
@@ -1,29 +1,38 @@
-edges = [[1, 2], [0, 2], [0, 1, 3, 4], [2, 4], [2, 3]]
+# edges = [[1, 2], [0, 2], [0, 1, 3, 4], [2, 4], [2, 3]]
 # edges[0].remove(1)
 # print(len(edges[0]))
 
-def Hierholzer(edges):
+def Hierholzer(adjacency_list):
     first = 0
     stack = []
     stack.append(first)
     ret = []
     while len(stack) > 0:
-        print("ret: ", ret)
         v = stack[-1]
-        print("v: ", v)
-        print("len(edges[v]): ", len(edges[v]))
-        if len(edges[v]) == 0:
+        if len(adjacency_list[v]) == 0:
             ret.append(v)
-            print("ret: ", ret)
             stack.pop()
-            print("stack: ", stack)
         else:
-            i = edges[v][- 1]
-            edges[v].pop()
+            i = adjacency_list[v][- 1]
+            adjacency_list[v].pop()
             # if v in edges[v]:
-            edges[i].remove(v)
+            adjacency_list[i].remove(v)
             stack.append(i)
 
     return ret
 
-print(Hierholzer(edges))
+
+def compute_adjacency_list(num_verts, edges):
+    adjacency_list = [[] for _ in range(num_verts)]
+
+    for i in range(len(edges) // 2):
+       id0 = edges[2 * i + 0]
+       id1 = edges[2 * i + 1]
+       adjacency_list[id0].append(id1)
+       adjacency_list[id1].append(id0)
+
+    return adjacency_list
+
+edges = [0, 1, 1, 2, 2, 0]
+adjacency_list = compute_adjacency_list(3, edges)
+print(Hierholzer(adjacency_list))
