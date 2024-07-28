fb6524452782facbace594b66d72433c2ef0852d
mhkee <minh702@korea.ac.kr>
2024-07-28 15:15:08 +0900

    unit test2. submodule

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ run_sim = False
 MODE_WIREFRAME = False
 LOOKAt_ORIGIN = True
 
-#test
+#test 2
 
 #selector
 g_selector = st.SelectionTool(sim.max_num_verts_dy, sim.mesh_dy.verts.x, window, camera)
