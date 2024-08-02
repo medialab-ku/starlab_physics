3e548afaa0daf0e23176b977d7ec1f6bb751d22d
gongpoman <wjdgmlwh1629343@gmail.com>
2024-08-02 16:04:01 +0900

    add fluid scene

---
+++ Scenes/fluid_test.py
@@ -0,0 +1,40 @@
+from framework.meshio.meshTaichiWrapper import MeshTaichiWrapper
+from framework.meshio.concat import concat_mesh
+import taichi as ti
+from pathlib import Path
+
+enable_profiler = False
+ti.init(arch=ti.cuda, device_memory_GB=8, kernel_profiler=enable_profiler)
+
+model_path = Path(__file__).resolve().parent.parent / "models"
+OBJ = "OBJ"
+model_dir = str(model_path) + "/OBJ"
+# print(model_dir)
+
+model_names = []
+trans_list = []
+scale_list = []
+
+concat_model_name = "concat.obj"
+#
+# model_names.append("poncho_8K.obj")
+# trans_list.append([0.0, 8.0, 0.0])
+# scale_list.append(3.0)
+#
+
+# append more meshes
+# model_names.append("your-obj-name.obj")
+# trans_list.append([x, y, z])
+# scale_list.append(size)
+
+# offsets = concat_mesh(concat_model_name, model_dir, model_names, trans_list, scale_list)
+offsets = []
+
+#dynamic mesh
+mesh_dy = MeshTaichiWrapper(model_dir, "torus.obj", offsets=offsets, scale=1.0, trans=ti.math.vec3(0, 0.0, 0), rot=ti.math.vec3(0.0, 0.0, 0.0))
+
+#static mesh
+mesh_st = MeshTaichiWrapper(model_dir, "SMPL_APose.obj",  offsets=[0], scale=12.0, trans=ti.math.vec3(0.0, 0.0, 0.01), rot=ti.math.vec3(0.0, 0.0, 0.0), is_static=True)
+
+# if you wan to use another mesh as a static object...
+# mesh_st = MeshTaichiWrapper(str(model_path / "OBJ/your-obj-name.obj"),  offsets=[0], scale=12.0, trans=ti.math.vec3(0.0, 0.0, 0.01), rot=ti.math.vec3(0.0, 0.0, 0.0), is_static=True)
