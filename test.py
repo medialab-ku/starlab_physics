3321b158d044e6ffd315971c030ac10585659a7c
mhkee <minh702@korea.ac.kr>
2024-08-22 21:27:21 +0900

    [wip] test 2d fluid

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ import math
 import numpy as np
 
 import taichi as ti
+import taichi.ui
 
 ti.init(arch=ti.gpu)
 
@@ -27,15 +28,17 @@ grid_size = (round_up(boundary[0], 1), round_up(boundary[1], 1))
 
 dim = 2
 bg_color = 0x112F41
-particle_color = 0x068587
-boundary_color = 0xEBACA2
+particle_color = ti.math.vec3(1.0, 1.0, 1.0)
+print(particle_color)
+boundary_particle_color = 0xFFC433
+# boundary_color = 0x068587
 num_particles_x = 60
 num_particles = num_particles_x * 20
 max_num_particles_per_cell = 50
 max_num_neighbors = 50
 time_delta = 1.0 / 20.0
 epsilon = 1e-5
-particle_radius = 3.0
+particle_radius = 10.0
 particle_radius_in_world = particle_radius / screen_to_world_ratio
 
 # PBF params
@@ -53,20 +56,20 @@ neighbor_radius = h_ * 1.05
 poly6_factor = 315.0 / 64.0 / math.pi
 spiky_grad_factor = -45.0 / math.pi
 
-old_positions = ti.Vector.field(dim, float)
-positions = ti.Vector.field(dim, float)
-velocities = ti.Vector.field(dim, float)
+# old_positions = ti.Vector.field(dim, float)
+# positions = ti.Vector.field(dim, float)
+# velocities = ti.Vector.field(dim, float)
 grid_num_particles = ti.field(int)
 grid2particles = ti.field(int)
 particle_num_neighbors = ti.field(int)
 particle_neighbors = ti.field(int)
-lambdas = ti.field(float)
-position_deltas = ti.Vector.field(dim, float)
+# lambdas = ti.field(float)
+# position_deltas = ti.Vector.field(dim, float)
 # # 0: x-pos, 1: timestep in sin()
 
-board_states = ti.Vector.field(2, float)
+board_states = ti.Vector.field(2, float, shape=())
 #
-ti.root.dense(ti.i, num_particles).place(old_positions, positions, velocities)
+# ti.root.dense(ti.i, num_particles).place(old_positions, positions, velocities)
 
 grid_snode = ti.root.dense(ti.ij, grid_size)
 grid_snode.place(grid_num_particles)
@@ -76,23 +79,35 @@ grid_snode.dense(ti.k, max_num_particles_per_cell).place(grid2particles)
 nb_node = ti.root.dense(ti.i, num_particles)
 nb_node.place(particle_num_neighbors)
 nb_node.dense(ti.j, max_num_neighbors).place(particle_neighbors)
-ti.root.dense(ti.i, num_particles).place(lambdas, position_deltas)
+# ti.root.dense(ti.i, num_particles).place(lambdas, position_deltas)
 
-print(particle_neighbors.shape)
-
-ti.root.place(board_states)
+# print(particle_neighbors.shape)
+# ti.root.place(board_states)
 # print(board_states.shape)
-# old_positions = ti.Vector.field(dim, float, shape=num_particles)
-# positions = ti.Vector.field(dim, float, shape=num_particles)
-# velocities = ti.Vector.field(dim, float, shape=num_particles)
+old_positions = ti.Vector.field(dim, float, shape=num_particles)
+positions = ti.Vector.field(dim, float, shape=num_particles)
+positions_window = ti.Vector.field(dim, float, shape=num_particles)
+colors = ti.Vector.field(dim, int, shape=num_particles)
+velocities = ti.Vector.field(dim, float, shape=num_particles)
 # grid_num_particles = ti.field(int, shape=grid_size)
 # grid2particles = ti.field(int, shape=(grid_size[0], grid_size[1], max_num_particles_per_cell))
 # particle_num_neighbors = ti.field(int, shape=num_particles)
 # particle_neighbors = ti.field(int)
-# lambdas = ti.field(float, shape=num_particles)
-# position_deltas = ti.Vector.field(dim, float, shape=num_particles)
-# board_states = ti.Vector.field(2, float)
+lambdas = ti.field(float, shape=num_particles)
+position_deltas = ti.Vector.field(dim, float, shape=num_particles)
 
+
+boundary_positions = ti.Vector.field(dim, float)
+boundary_positions_window = ti.Vector.field(dim, float)
+num_boundary_particles_x = 40
+num_boundary_particles = 10 * num_boundary_particles_x
+ti.root.dense(ti.i, num_boundary_particles).place(boundary_positions, boundary_positions_window)
+
+test = ti.Vector.field(2, float, shape=2)
+test[0] = ti.math.vec2(0.5)
+test[1] = ti.math.vec2(1.0)
+
+# board_states = ti.Vector.field(2, float)
 @ti.func
 def poly6_value(s, h):
     result = 0.0
@@ -136,8 +151,10 @@ def is_in_grid(c):
 
 @ti.func
 def confine_position_to_boundary(p):
-    bmin = particle_radius_in_world
-    bmax = ti.Vector([board_states[None][0], boundary[1]]) - particle_radius_in_world
+    bmin = h_
+    bmax = ti.Vector([board_states[None][0], boundary[1]]) - h_
+
+    # print(bmax)
     for i in ti.static(range(dim)):
         # Use randomness to prevent particles from sticking into each other after clamping
         if p[i] <= bmin:
@@ -200,8 +217,6 @@ def prologue():
                         particle_neighbors[p_i, nb_i] = p_j
                         nb_i += 1
         particle_num_neighbors[p_i] = nb_i
-
-
 @ti.kernel
 def substep():
     # compute lambdas
@@ -222,10 +237,8 @@ def substep():
             sum_gradient_sqr += grad_j.dot(grad_j)
             # Eq(2)
             density_constraint += poly6_value(pos_ji.norm(), h_)
-
         # Eq(1)
         density_constraint = (mass * density_constraint / rho0) - 1.0
-
         sum_gradient_sqr += grad_i.dot(grad_i)
         lambdas[p_i] = (-density_constraint) / (sum_gradient_sqr + lambda_epsilon)
     # compute position deltas
@@ -260,6 +273,8 @@ def epilogue():
     # update velocities
     for i in positions:
         velocities[i] = (positions[i] - old_positions[i]) / time_delta
+
+
     # no vorticity/xsph because we cannot do cross product in 2D...
 
 
@@ -269,30 +284,61 @@ def run_pbf():
         substep()
     epilogue()
 
+@ti.kernel
+def render_kernel(pos: ti.template(), pos_window: ti.template(), ratio: float, res_x: float, res_y: float):
+    # print(ratio, res_x, res_y)
+    # print(pos[0][0])
+
+    for i in pos:
+        pos_window[i][0] = (ratio / res_x) * pos[i][0]
+        pos_window[i][1] = (ratio / res_x) * pos[i][1]
 
 def render(gui):
-    gui.clear(bg_color)
-    pos_np = positions.to_numpy()
-    for j in range(dim):
-        pos_np[:, j] *= screen_to_world_ratio / screen_res[j]
-    gui.circles(pos_np, radius=particle_radius, color=particle_color)
-    gui.rect(
-        (0, 0),
-        (board_states[None][0] / boundary[0], 1),
-        radius=1.5,
-        color=boundary_color,
-    )
-    gui.show()
-
+    # gui.clear(bg_color)
+    render_kernel(positions, positions_window, screen_to_world_ratio, screen_res[0], screen_res[1])
+    # pos_np = positions.to_numpy()
+    # boundary_pos_np = boundary_positions.to_numpy()
+    # for j in range(dim):
+    #     pos_np[:, j] *= screen_to_world_ratio / screen_res[j]
+    #     boundary_pos_np[:, j] *= screen_to_world_ratio / screen_res[j]
+    #
+    # colors.fill(1.0)
+    gui.circles(positions_window, radius=particle_radius,  per_vertex_color=colors)
+    # gui.circles(boundary_pos_np, radius=particle_radius, color=boundary_particle_color)
+    # gui.rect(
+    #     (0, 0),
+    #     (board_states[None][0] / boundary[0], 1),
+    #     radius=1.5,
+    #     color=boundary_color,
+    # )
+    # gui.show()
 
 @ti.kernel
 def init_particles():
+
+    print(boundary)
+
     for i in range(num_particles):
-        delta = h_ * 0.8
-        offs = ti.Vector([(boundary[0] - delta * num_particles_x) * 0.5, boundary[1] * 0.02])
-        positions[i] = ti.Vector([i % num_particles_x, i // num_particles_x]) * delta + offs
-        for c in ti.static(range(dim)):
-            velocities[i][c] = (ti.random() - 0.5) * 4
+        delta = h_ * 0.6
+        offs = ti.Vector([(boundary[0] - delta * num_particles_x) * 0.5, boundary[1] * 0.4])
+        y = i // num_particles_x
+        # if 0 <= y <=1:
+        #     colors[i] = ti.math.
+        colors[i] = 0x068587
+        positions[i] = ti.Vector([i % num_particles_x, y]) * delta + offs
+        # for c in ti.static(range(dim)):
+        #     velocities[i][c] = (ti.random() - 0.5) * 4
+
+    for i in range(num_boundary_particles):
+
+        delta = h_ * 0.6
+        offs = ti.Vector([(boundary[0] - delta * num_boundary_particles_x) * 0.5, boundary[1] * 0.2])
+        boundary_positions[i] = ti.Vector([i % num_boundary_particles_x, i // num_boundary_particles_x]) * delta + offs
+        # for c in ti.static(range(dim)):
+        #     velocities[i][c] = (ti.random() - 0.5) * 4
+
+
+    velocities.fill(0.0)
     board_states[None] = ti.Vector([boundary[0] - epsilon, -0.0])
 
 
@@ -307,16 +353,44 @@ def print_stats():
 
 
 def main():
+    ones = np.ones([3, 2])
+    for j in range(dim):
+        ones[:, j] *= screen_to_world_ratio / screen_res[j]
+
+    print(ones)
+
+    run_sim = False
     init_particles()
     print(f"boundary={boundary} grid={grid_size} cell_size={cell_size}")
-    gui = ti.GUI("PBF2D", screen_res)
-    while gui.running and not gui.get_event(gui.ESCAPE):
-        move_board()
-        run_pbf()
-        # if gui.frame % 20 == 1:
-        #     print_stats()
-        render(gui)
 
+    window = ti.ui.Window(name='PBF2D', res = screen_res, fps_limit=200, pos = (150, 150))
+    canvas = window.get_canvas()
+    canvas.set_background_color((0.2, 0.2, 0.2))
+    # gui = ti.GUI("PBF2D", screen_res)
+    # ggui = ti.GG
+    while window.running:
+
+        if window.get_event(ti.ui.PRESS):
+            if window.event.key == ' ':
+                run_sim = not run_sim
+
+            if window.event.key == 'r':
+                init_particles()
+                run_sim = False
+
+        if run_sim:
+            run_pbf()
+
+        render_kernel(positions, positions_window, screen_to_world_ratio, screen_res[0], screen_res[1])
+        render_kernel(boundary_positions, boundary_positions_window, screen_to_world_ratio, screen_res[0], screen_res[1])
+
+        radius = 0.5 * (screen_to_world_ratio / screen_res[0]) * h_
+
+        # canvas.circles(positions, radius=0.1, color=(1.0, 1.0, 1.0))
+        canvas.circles(positions_window, radius=radius, color=(0.0/255.0, 153.0/255.0, 153.0/255.0))
+        # canvas.circles(boundary_positions, radius=radius, color=(1.0, 1.0, 1.0))
+        # render(canvas)
+        window.show()
 
 if __name__ == "__main__":
     main()
