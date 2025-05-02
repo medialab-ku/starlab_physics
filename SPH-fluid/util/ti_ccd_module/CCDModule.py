0dcf20c668571edd716ad7c9f84bef39787bca31
Rascal0902 <geonho050902@gmail.com>
2025-05-02 22:44:20 +0900

    edge-edge CCD

--- SPH-fluid/util/ti_ccd_module/CCDModule.py
+++ SPH-fluid/util/ti_ccd_module/CCDModule.py
@@ -22,6 +22,12 @@ class CCDModule:
         return (aTb * aTb) / b.norm_sqr()
 
     @ti.func
+    def edge_edge_distance(self, v0, v1, v2, v3):
+        u = v1 - v0; v = v3 - v2; b = u.cross(v)
+        aTb = (v2 - v0).dot(b)
+        return (aTb * aTb) / b.norm_sqr()
+
+    @ti.func
     def classify_edge(self, p, a, b, normal):
         basis0 = b - a
         basis1 = basis0.cross(normal)
@@ -60,6 +66,46 @@ class CCDModule:
         return res
 
     @ti.func
+    def _dtype_edge_edge(self, v0, v1, v2, v3):
+        _v0 = ti.cast(v0, ti.f64); _v1 = ti.cast(v1, ti.f64); _v2 = ti.cast(v2, ti.f64); _v3 = ti.cast(v3, ti.f64)
+        u = _v1 - _v0; v = _v3 - _v2; w = _v0 - _v2
+        a = u.dot(u); b = u.dot(v); c = v.dot(v)
+        d = u.dot(w); e = v.dot(w)
+
+        D = a * c - b * b; tD = D
+        sN = b * e - c * d
+        tN = ti.cast(0.0, ti.f64)
+        result = 8
+
+        if sN <= 0.0:
+            tN = e; tD = c
+            result = 2
+        elif sN >= D:
+            tN = e + b; tD = c
+            result = 5
+        else:
+            tN = a * e - b * d
+            cross_uv = u.cross(v)
+            if tN > 0.0 and tN < tD and (w.dot(cross_uv) == 0.0 or cross_uv.norm_sqr() < 1.0e-20 * a * c):
+                if sN < D * 0.5:
+                    tN = e; tD = c
+                    result = 2
+                else:
+                    tN = e + b; tD = c
+                    result = 5
+
+        if tN <= 0.0:
+            if -d <= 0.0: result = 0
+            elif -d >= a: result = 3
+            else: result = 6
+        elif tN >= tD:
+            if -d + b <= 0.0: result = 1
+            elif -d + b >= a: result = 4
+            else: result = 7
+
+        return result
+
+    @ti.func
     def point_triangle_distance_unclassified(self, p, t0, t1, t2):
         dtype = self.dtype_point_triangle(p, t0, t1, t2)
         res = ti.cast(1e32, ti.f64)
@@ -70,6 +116,21 @@ class CCDModule:
         elif dtype == 4: res = self.point_edge_distance(p, t1, t2)
         elif dtype == 5: res = self.point_edge_distance(p, t2, t0)
         elif dtype == 6: res = self.point_triangle_distance(p, t0, t1, t2)
+        return res
+
+    @ti.func
+    def edge_edge_distance_unclassified(self, ea0, ea1, eb0, eb1):
+        dtype = self._dtype_edge_edge(ea0, ea1, eb0, eb1)
+        res = ti.cast(1e32, ti.f64)
+        if dtype == 0: res = self.point_point_distance(ea0, eb0)
+        elif dtype == 1: res = self.point_point_distance(ea0, eb1)
+        elif dtype == 2: res = self.point_edge_distance(ea0, eb0, eb1)
+        elif dtype == 3: res = self.point_point_distance(ea1, eb0)
+        elif dtype == 4: res = self.point_point_distance(ea1, eb1)
+        elif dtype == 5: res = self.point_edge_distance(ea1, eb0, eb1)
+        elif dtype == 6: res = self.point_edge_distance(eb0, ea0, ea1)
+        elif dtype == 7: res = self.point_edge_distance(eb1, ea0, ea1)
+        elif dtype == 8: res = self.edge_edge_distance(ea0, ea1, eb0, eb1)
         return res
 
     @ti.func
@@ -93,7 +154,6 @@ class CCDModule:
 
             while count <= 50000:
                 count += 1
-
                 toc_lower_bound = (1 - eta) * (dist2_cur - thickness * thickness) / (
                             (dist_cur + thickness) * max_disp_mag)
 
@@ -113,6 +173,53 @@ class CCDModule:
 
         return toc
 
+    @ti.func
+    def edge_edge_ccd(self, ea0, ea1, eb0, eb1, dea0, dea1, deb0, deb1, eta: ti.f64, thickness: ti.f64):
+        ea0 = ti.cast(ea0, ti.f64); ea1 = ti.cast(ea1, ti.f64); eb0 = ti.cast(eb0, ti.f64); eb1 = ti.cast(eb1, ti.f64)
+        dea0 = ti.cast(dea0, ti.f64); dea1 = ti.cast(dea1, ti.f64); deb0 = ti.cast(deb0, ti.f64); deb1 = ti.cast(deb1, ti.f64)
+
+        mov = -0.25 * (dea0 + dea1 + deb0 + deb1)
+        dea0 += mov; dea1 += mov; deb0 += mov; deb1 += mov
+
+        max_disp_mag = (ti.sqrt(ti.max(dea0.norm_sqr(), dea1.norm_sqr()))
+                        + ti.sqrt(ti.max(deb0.norm_sqr(), deb1.norm_sqr())))
+        toc = ti.cast(1.0, ti.f64)
+
+        if max_disp_mag != 0.0:
+            dist2_cur = self.edge_edge_distance_unclassified(ea0, ea1, eb0, eb1)
+            dFunc = dist2_cur - thickness * thickness
+
+            if dFunc <= 0.0:
+                dist2_cur = min((ea0 - eb0).norm_sqr(), (ea0 - eb1).norm_sqr(),
+                                (ea1 - eb0).norm_sqr(), (ea1 - eb1).norm_sqr())
+                dFunc = dist2_cur - thickness * thickness
+            dist_cur = ti.sqrt(dist2_cur)
+            gap = eta * dFunc / (dist_cur + thickness)
+
+            toc = 0.0
+            count = 0
+
+            while count <= 50000:
+                count += 1
+                toc_lower_bound = (1.0 - eta) * dFunc / ((dist_cur + thickness) * max_disp_mag)
+
+                ea0 += dea0 * toc_lower_bound; ea1 += dea1 * toc_lower_bound
+                eb0 += deb0 * toc_lower_bound; eb1 += deb1 * toc_lower_bound
+
+                dist2_cur = self.edge_edge_distance_unclassified(ea0, ea1, eb0, eb1)
+                dFunc = dist2_cur - thickness * thickness
+
+                if dFunc <= 0.0:
+                    dist2_cur = min((ea0 - eb0).norm_sqr(), (ea0 - eb1).norm_sqr(),
+                                    (ea1 - eb0).norm_sqr(), (ea1 - eb1).norm_sqr())
+                    dFunc = dist2_cur - thickness * thickness
+                dist_cur = ti.sqrt(dist2_cur)
+
+                if toc > 0.0 and (dFunc / (dist_cur + thickness)) < gap:break
+                toc += toc_lower_bound
+                if toc > 1.0: toc = 1.0; break
+        return toc
+
     # ToDo temp kernel code
     @ti.kernel
     def point_mesh_ccd(self, p: ti.template(), dp: ti.template(),
@@ -127,6 +234,25 @@ class CCDModule:
             dt0 = tri_moves[i, 0]; dt1 = tri_moves[i, 1]; dt2 = tri_moves[i, 2]
 
             toc = self.point_triangle_ccd(p, t0, t1, t2, dp, dt0, dt1, dt2, eta, thickness)
+            if toc < min_toc:
+                min_toc = toc
+
+        return min_toc
+
+    # ToDo temp kernel code
+    @ti.kernel
+    def edges_edges_ccd(self, edges_a: ti.template(), moves_a: ti.template(),
+                        edges_b: ti.template(), moves_b: ti.template(),
+                        eta: ti.f64, thickness: ti.f64) -> ti.f64:
+
+        N = edges_a.shape[0]; M = edges_b.shape[0]
+        min_toc = ti.cast(1.0, ti.f64)
+
+        for i, j in ti.ndrange(N, M):  # 2차원 병렬 루프
+            ea0 = edges_a[i, 0]; ea1 = edges_a[i, 1]; dea0 = moves_a[i, 0]; dea1 = moves_a[i, 1]
+            eb0 = edges_b[j, 0]; eb1 = edges_b[j, 1]; deb0 = moves_b[j, 0]; deb1 = moves_b[j, 1]
+
+            toc = self.edge_edge_ccd(ea0, ea1, eb0, eb1, dea0, dea1, deb0, deb1, eta, thickness)
             if toc < min_toc:
                 min_toc = toc
 
--- SPH-fluid/util/ti_ccd_module/run_CCD_example.py
+++ SPH-fluid/util/ti_ccd_module/run_CCD_example.py
@@ -7,13 +7,13 @@ if __name__ == '__main__':
     ccd = CCDModule()
 
     ####################################
-    # ToDo initial setting
+    # ToDo initial setting (point-triangle)
     # one triangle
     N = 1
     triangles = ti.Vector.field(3, dtype=ti.f64, shape=(N, 3))
     tri_moves = ti.Vector.field(3, dtype=ti.f64, shape=(N, 3))
 
-    # initial point
+    # initial points
     p = ti.Vector([0.0, 0.0, 1.0], dt=ti.f64)
     t0 = ti.Vector([0.0, -1.0, 0.0], dt=ti.f64)
     t1 = ti.Vector([1.0, 1.0, 1.0], dt=ti.f64)
@@ -35,4 +35,40 @@ if __name__ == '__main__':
     ####################################
 
     toc = ccd.point_mesh_ccd(p, dp, triangles, tri_moves, eta, thickness)
-    print("Time of Contact (toc):", toc)
+    print("point-triangle Time of Contact (toc):", toc)
+
+    ####################################
+    # ToDo: initial setting (edge-edge)
+    # one edge pair
+    N = 1; M = 1
+    edges_a = ti.Vector.field(3, dtype=ti.f64, shape=(N, 2))
+    edges_b = ti.Vector.field(3, dtype=ti.f64, shape=(M, 2))
+    moves_a = ti.Vector.field(3, dtype=ti.f64, shape=(N, 2))
+    moves_b = ti.Vector.field(3, dtype=ti.f64, shape=(M, 2))
+
+    # initial edges
+    ea0 = ti.Vector([0.0, -1.0, 0.0], dt=ti.f64)
+    ea1 = ti.Vector([0.0, 1.0, 0.0], dt=ti.f64)
+    eb0 = ti.Vector([-1.0, 0.0, -0.3], dt=ti.f64)
+    eb1 = ti.Vector([1.0, 0.0, -0.7], dt=ti.f64)
+
+    # movement
+    dea0 = ti.Vector([0.0, 0.0, -1.0], dt=ti.f64)
+    dea1 = ti.Vector([0.0, 0.0, -1.0], dt=ti.f64)
+    deb0 = ti.Vector([0.0, 0.0, 0.0], dt=ti.f64)
+    deb1 = ti.Vector([-1.0, 0.0, 0.0], dt=ti.f64)
+
+    # array initialization
+    edges_a[0, 0] = ea0; edges_a[0, 1] = ea1
+    edges_b[0, 0] = eb0; edges_b[0, 1] = eb1
+    moves_a[0, 0] = dea0; moves_a[0, 1] = dea1
+    moves_b[0, 0] = deb0; moves_b[0, 1] = deb1
+
+    # CCD parameter
+    eta = 0.01
+    thickness = 0.01
+    ####################################
+
+    # CCD 실행
+    toc = ccd.edges_edges_ccd(edges_a, moves_a, edges_b, moves_b, eta, thickness)
+    print("edge-edge Time of Contact (toc):", toc)
