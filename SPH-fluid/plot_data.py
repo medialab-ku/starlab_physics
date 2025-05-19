b92324d5570d0096678d4528a3c2159ae28e4914
minh702 <minh702@korea.ac.kr>
2025-05-20 01:48:27 +0900

    [save] divergence free test

--- SPH-fluid/plot_data.py
+++ SPH-fluid/plot_data.py
@@ -4,24 +4,35 @@ import matplotlib.pyplot as plt
 def load_pcg_iter_from_json(json_path):
     with open(json_path, 'r') as f:
         data = json.load(f)
-        pcg_iter = data["residual_data"]["pcg_iter"]
+        pcg_iter = data["residual_data"]["opt_iter"]
 
         return pcg_iter
 
-file1 = "./data/results/20250519_222827.json"
-file2 = "./data/results/20250519_221831.json"
+
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.serif": ["Times New Roman"],
+    "text.latex.preamble": r"""
+        \usepackage{newtxtext,newtxmath}
+    """,
+})
+
+file1 = "./data/results/1.01.json"
+file2 = "./data/results/1.001.json"
 
 pcg_iter_1 = load_pcg_iter_from_json(file1)
 pcg_iter_2 = load_pcg_iter_from_json(file2)
 
 plt.figure(figsize=(8, 5))
-plt.plot(pcg_iter_1, label='File 1', linestyle='-', linewidth=1)
-plt.plot(pcg_iter_2, label='File 2', linestyle='--', linewidth=1)
+plt.plot(pcg_iter_1, label=r"$\gamma = 1.01$", linestyle='-', linewidth=1)
+plt.plot(pcg_iter_2, label=r"$\gamma = 1.001$", linestyle='-', linewidth=1)
 
-plt.xlabel("Frame")
-plt.ylabel("PCG Iteration Count")
-plt.title("Comparison of PCG Iterations")
+plt.xlabel("Frame", fontsize=20)
+plt.ylim(0, 400)
+plt.ylabel("Solver iteration", fontsize=20)
+# plt.title("Comparison of PCG Iterations")
 plt.legend()
-plt.grid(True)
+# plt.grid(True)
 plt.tight_layout()
 plt.show()
